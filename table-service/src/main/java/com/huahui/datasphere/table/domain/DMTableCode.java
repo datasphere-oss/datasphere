6fb7bbaf153a4db7b53f3f17cf484fc386fb0bd1
theseusyang <theseusyang@gmail.com>
2021-09-20 17:02:52 +0800

    add DMTableCode

---
+++ table-service/src/main/java/com/huahui/datasphere/table/domain/DMTableCode.java
@@ -0,0 +1,121 @@
+/*
+ * Apache License
+ * 
+ * Copyright (c) 2020 HuahuiData
+ * 
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.huahui.datasphere.table.domain;
+
+import java.io.Serializable;
+import java.sql.Date;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+public class DMTableCode implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+
+	
+	/**
+	* 检查给定的名字，是否为主键中的字段
+	* @param name String 检验是否为主键的名字
+	* @return
+	*/
+
+	/** 数据对标码值信息表 */
+	
+	// 分类代码
+	protected String classifiedCode;
+	// 表模式
+	protected String tableSchema;
+	// 表代码
+	protected String tableCode;
+	// 列代码
+	protected String columnCode;
+	// 码值类型
+	protected String valueType;
+	// 代码分类
+	protected String category;
+	
+	// 码值
+	protected String value;
+	// 开始时间
+	private Date startDate;
+	// 代码描述
+	private String description;
+
+	public String getClassifiedCode() {
+		return classifiedCode;
+	}
+	public void setClassifiedCode(String classifiedCode) {
+		this.classifiedCode = classifiedCode;
+	}
+	public String getTableSchema() {
+		return tableSchema;
+	}
+	public void setTableSchema(String tableSchema) {
+		this.tableSchema = tableSchema;
+	}
+	public String getTableCode() {
+		return tableCode;
+	}
+	public void setTableCode(String tableCode) {
+		this.tableCode = tableCode;
+	}
+	public String getColumnCode() {
+		return columnCode;
+	}
+	public void setColumnCode(String columnCode) {
+		this.columnCode = columnCode;
+	}
+	public String getValueType() {
+		return valueType;
+	}
+	public void setValueType(String valueType) {
+		this.valueType = valueType;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public String getValue() {
+		return value;
+	}
+	public void setValue(String value) {
+		this.value = value;
+	}
+	public Date getStartDate() {
+		return startDate;
+	}
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+
+}
